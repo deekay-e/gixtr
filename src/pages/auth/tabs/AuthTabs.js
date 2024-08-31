511cdbbee06c92ad381732300f3e68cd5990d79f
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-08-31 13:35:19 +0100

    feat: add background image

--- src/pages/auth/tabs/AuthTabs.js
+++ src/pages/auth/tabs/AuthTabs.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './AuthTabs.scss'
+import backgroundImage from '../../../assets/images/background.jpg'
 
 const AuthTabs = () => {
+  const [type, setType] = useState('Sign In')
   return (
     <>
-      <div className="container-wrapper">
+      <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
         <div className="environment">DEV</div>
         <div className="container-wrapper-auth">
           <div className="tabs">
             <div className="tabs-auth">
               <ul className="tab-group">
-                <li className='tab active'>
+                <li className="tab active" onClick={() => setType('Sign In')}>
                   <button className="login">Sign In</button>
                 </li>
-                <li className='tab'>
+                <li className="tab" onClick={() => setType('Sign Up')}>
                   <button className="signup">Sign Up</button>
                 </li>
               </ul>
-              <div className="tab-item">
-                login component
-              </div>
+              {type === 'Sign In' && <div className="tab-item">login component</div>}
+              {type === 'Sign Up' && <div className="tab-item">register component</div>}
             </div>
           </div>
         </div>

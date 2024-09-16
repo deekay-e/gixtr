c2b8793d1dd252d1a184011d2b9305777a9b6590
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-09-16 18:38:42 +0100

    feat: connect signup to backend api

--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -1,46 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Register.scss'
 import Input from '../../../components/input/Input'
 import Button from '../../../components/button/Button'
 
 const Register = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [alertType, setAlertType] = useState('')
+  const [hasError, setHasError] = useState(false)
+
+  const registerUser = async (event) => {
+    setLoading(true)
+    event.preventDefault()
+    try {
+      setHasError(false)
+      setAlertType('alert-success')
+    } catch (error) {
+      setLoading(false)
+      setHasError(true)
+      setAlertType('alert-error')
+      setErrorMessage(error?.response?.data.message)
+    }
+  }
+
   return (
     <div className="auth-inner">
-      {/* <div className="alerts alert-error" role="alert">
-        Error message
-      </div> */}
-      <form className="auth-form">
+      {hasError && errorMessage && (
+        <div className={`alerts ${alertType}`} role="alert">
+          {errorMessage}
+        </div>
+      )}
+      <form className="auth-form" onSubmit={registerUser}>
         <div className="form-input-container">
           <Input
             id="username"
             name="username"
             type="text"
-            value="KayCee"
+            value={username}
             labelText="Username"
             placeholder="Enter your username"
-            handleChange={() => {}}
+            handleChange={(event) => setUsername(event.target.value)}
           />
           <Input
             id="email"
             name="email"
             type="email"
-            value="chi.pra@kaycee.co"
+            value={email}
             labelText="Email"
             placeholder="Enter your email"
-            handleChange={() => {}}
+            handleChange={() => setEmail(event.target.value)}
           />
           <Input
             id="password"
             name="password"
             type="password"
-            value="praise"
+            value={password}
             labelText="Password"
             placeholder="Enter your password"
-            handleChange={() => {}}
+            handleChange={() => setPassword(event.target.value)}
           />
         </div>
-        <Button label={'SIGN UP'} className="auth-button button" disabled={true} />
+        <Button
+          label={`${loading ? 'SIGNUP IN PROGRESS...' : 'SIGN UP'}`}
+          className="auth-button button"
+          disabled={!username || !email || !password}
+        />
       </form>
     </div>
   )
---
+++ src/services/api/auth/auth.service.js
@@ -0,0 +1,31 @@
+import axios from '../../axios'
+
+class AuthService {
+  /* - use this generic method as alternative
+    async postRequests(url, data) {
+      return await axios.post(url, data)
+    }
+  */
+
+  async signup(body) {
+    const res = await axios.post('/signup', body)
+    return res
+  }
+
+  async signin(body) {
+    const res = await axios.post('/signin', body)
+    return res
+  }
+
+  async forgotPassword(email) {
+    const res = await axios.post('/forgot-password', { email })
+    return res
+  }
+
+  async resetPassword(token, body) {
+    const res = await axios.post(`/reset-password/${token}`, body)
+    return res
+  }
+}
+
+export const authService = new AuthService()
